c45f59f217cc7e56b9b5807ad1d95e2b6bc7e635
weicong317 <weicong317@gmail.com>
2018-07-19 10:09:38 +0800

    added rspec to the cookie

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rspec'
 
 require_relative 'cookie'
@@ -23,16 +22,19 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
-
+    it "returns the type of the cookie" do
+      expect(cookie.type).to eq(type)
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect {cookie.bake!("2")}.to raise_error(TypeError)
+    end
 
-
-    it "returns the cookie object"
-
+    it "returns the cookie object" do
+      expect(cookie.bake!(1)).to be_a_kind_of(Cookie)
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
@@ -40,32 +42,38 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
-
+    it "returns the cookie's current status" do
+      expect(cookie.status).to eq(:doughy)      
+    end
 
     context "when unbaked" do
-      it "is `:doughy`"
-
+      it "is `:doughy`" do
+        expect(cookie.bake!(0).status).to eq(:doughy)      
+      end
     end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-
+      it "is `:doughy`" do
+        expect(cookie.bake!(6).status).to eq(:doughy)      
+      end
     end
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
-
+      it "is `:almost_ready`" do
+        expect(cookie.bake!(9).status).to eq(:almost_ready)      
+      end
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
-
+      it "is `:ready`" do
+        expect(cookie.bake!(11).status).to eq(:ready)              
+      end
     end
 
     context "when baked for at least 12 minutes" do
-      it "is `:burned`"
-
+      it "is `:burned`" do
+        expect(cookie.bake!(12).status).to eq(:burned)      
+      end
     end
   end
 end
